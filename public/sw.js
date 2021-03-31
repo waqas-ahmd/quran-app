8597d4c8448f10993842763fbe186497421f8ceb
waqas-ahmd <65waqas@gmail.com>
2021-03-31 21:14:16 +0500

    service workers added

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+let cacheData = "cache1";
+
+this.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheData).then((cache) =>
+      cache.addAll([
+        //deployed site:
+        // "https://cocky-galileo-58f5a9.netlify.app/",
+        // "/static/css/main.21ca11e7.chunk.css",
+        // "/static/js/2.d8b0c408.chunk.js",
+        // "/static/js/main.1cb9d11d.chunk.js",
+        "manifest.json",
+        "sw.js",
+        "logo192.png",
+        "/favicon.ico",
+      ])
+    )
+  );
+});
+
+this.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.open(cacheData).then(function (cache) {
+      return cache.match(event.request).then((response) => {
+        return (
+          response ||
+          fetch(event.request).then((response) => {
+            // cache.put(event.request, response.clone());
+            return response;
+          })
+        );
+      });
+    })
+  );
+});
