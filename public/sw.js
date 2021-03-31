0f0bfcd688f2f5e2e10f2a8c979ad19581d705d1
waqas-ahmd <65waqas@gmail.com>
2021-03-31 22:36:12 +0500

    sw edit

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,28 @@
 let cacheData = "cache1";
 
+var arrayOfImages = [null];
+
+for (var i = 1; i <= 550; i++) {
+  var num = null;
+  if (i < 10) {
+    num = "000" + i;
+  } else if (i < 100) {
+    num = "00" + i;
+  } else if (i < 1000) {
+    num = "0" + i;
+  }
+  arrayOfImages[i] = `/page-images/${num}.png`;
+}
+
 this.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(cacheData).then((cache) =>
       cache.addAll([
         //deployed site:
+        ...arrayOfImages,
         "https://quran16.netlify.app/",
         "/static/css/main.98f2faf0.chunk.css",
         "/static/js/2.b02f468c.chunk.js",
-        // "/page-images/*",
         "/static/media/mosque.1a799f13.jpg",
         "manifest.json",
         "sw.js",
